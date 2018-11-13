b9f6b3f81d98ac6557461daeb62fc508a7d93f1a
kamataryo <mugil.cephalus+ciconia.local@gmail.com>
2018-11-13 12:35:20 +0900

    Add useState sample

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { render } from 'react-dom'
 
 const App = () => {
-  return 'hello'
+  const [state, setState] = useState({ count: 1 })
+
+  return (
+    <div>
+      <span>{state.count}</span>
+      <button onClick={() => setState({ count: state.count + 1 })}>
+        {'count up'}
+      </button>
+    </div>
+  )
 }
 
 const root = document.getElementById('app')
-
 render(<App />, root)
